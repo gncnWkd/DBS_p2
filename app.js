39e06cdd209503b43e161fd29bd1d0d1ca1f578f
gncnWkd <hyunchan7764@naver.com>
2024-11-07 17:22:09 +0900

    로그인 로그아웃 회원가입 회원탈퇴 완료

--- app.js
+++ app.js
@@ -33,10 +33,14 @@ db.connect((err) => {
 
 // localhost:8000 접속시 로그인페이지로
 app.get('/', (req, res) => {
-  res.render('login')
+  if(req.session.user) {
+    res.render('home', {sessionInfo: req.session.user})
+  } else {
+    res.render('login')
+  }
 });
 
-// localhost:8000 접속시 로그인페이지로
+// 홈 페이지 라우트
 app.get('/home', (req, res) => {
   if(req.session.user) {
     res.render('home', {sessionInfo: req.session.user})
@@ -47,21 +51,69 @@ app.get('/home', (req, res) => {
 
 // 로그인 페이지 라우트
 app.get('/login', (req, res) => {
-  res.render('login');
+  if(req.session.user) {
+    res.send(`
+        <html>
+            <head>
+                <title>이미 로그인 중</title>
+            </head>
+            <body>
+                <h1>이미 로그인되어있습니다.</h1>
+                <p>3초 뒤 홈 화면으로 돌아갑니다.</p>
+                <script>
+                    // 3000ms (3초) 뒤에 '/home' 경로로 리디렉트
+                    setTimeout(() => {
+                        window.location.href = '/home';
+                    }, 3000);
+                </script>
+            </body>
+        </html>`);
+  } else {
+    res.render('login');
+  }
 });
 
-//회원가입 페이지 라우트
+// 회원가입 페이지 라우트
 app.get('/register', (req, res) => {
-  res.render('register');
+  if(req.session.user) {
+    res.send(`
+        <html>
+            <head>
+                <title>이미 로그인 중</title>
+            </head>
+            <body>
+                <h1>이미 로그인되어있습니다.</h1>
+                <p>3초 뒤 홈 화면으로 돌아갑니다.</p>
+                <script>
+                    // 3000ms (3초) 뒤에 '/home' 경로로 리디렉트
+                    setTimeout(() => {
+                        window.location.href = '/home';
+                    }, 3000);
+                </script>
+            </body>
+        </html>`);
+  } else {
+    res.render('register');
+  }
+})
+
+// 회원탈퇴 페이지 라우트
+app.get('/deleteAccount', (req, res) => {
+  if(req.session.user) {
+    res.render('deleteAccount')
+  } else {
+    res.render('login');
+  }
 })
 
 // 예수금 입금/출금 페이지 라우트
 app.get('/balanceInputOutput', (req, res) => {
-  res.render('balanceInputOutput');
+  if(req.session.user) {
+    res.render('balanceInputOutput');
+  } else {
+    res.render('login')
+  }
 })
-
-
-
 
 
 
@@ -91,17 +143,105 @@ app.post('/login', (req, res) => {
             </head>
             <body>
                 <h1>아이디나 비밀번호가 틀렸습니다.</h1>
-                <p>3초 뒤 홈화면으로 돌아갑니다.</p>
+                <p>3초 뒤 로그인 화면으로 돌아갑니다.</p>
                 <script>
-                    // 3000ms (3초) 뒤에 '/home' 경로로 리디렉트
+                    // 3000ms (3초) 뒤에 '/login' 경로로 리디렉트
                     setTimeout(() => {
-                        window.location.href = '/home';
+                        window.location.href = '/login';
                     }, 3000);
                 </script>
             </body>
         </html>`);
     }
   });
+});
+
+// 로그아웃 기능
+app.post('/logout', (req, res) => {
+  req.session.destroy();
+  res.send(`
+    <html>
+        <head>
+            <title>로그아웃</title>
+        </head>
+        <body>
+            <h1>로그아웃 완료</h1>
+            <p>3초 뒤 로그인 화면으로 돌아갑니다.</p>
+            <script>
+                // 3000ms (3초) 뒤에 '/login' 경로로 리디렉트
+                setTimeout(() => {
+                    window.location.href = '/login';
+                }, 3000);
+            </script>
+        </body>
+    </html>`);
+});
+
+// 회원가입 기능
+app.post('/register', (req, res) => {
+  const {UserID, Password, Name} = req.body;
+  db.query('SELECT * FROM user WHERE UserID = ?', [UserID], (err, results) => {
+    if(err) throw err;
+
+    if(results.length <= 0) {
+      db.query('INSERT INTO user VALUE(?, ?, ?, 0)', [UserID, Password, Name], (err, results) => {
+        if(err) throw err;
+
+        res.send(`
+          <html>
+              <head>
+                  <title>회원가입 성공</title>
+              </head>
+              <body>
+                  <h1>회원가입에 성공하였습니다.</h1>
+                  <a href = "/login">로그인 페이지</a>
+              </body>
+          </html>`);
+      });
+      
+    } else {
+      res.send(`
+        <html>
+            <head>
+                <title>회원가입 실패</title>
+            </head>
+            <body>
+                <h1>이미 존재하는 아이디입니다.</h1>
+                <a href = "/register">회원가입 페이지</a>
+            </body>
+        </html>`);
+    }
+  });
+});
+
+// 회원탈퇴 기능
+app.post('/deleteAccount_YES', (req, res) => {
+  db.query('DELETE FROM user WHERE UserID = ?', [req.session.user.UserID], (err, results) => {
+    if(err) throw err;
+
+    req.session.destroy();
+    res.send(`
+      <html>
+          <head>
+              <title>회원탈퇴 성공</title>
+          </head>
+          <body>
+              <h1>회원탈퇴하였습니다.</h1>
+              <p>3초 뒤 로그인 화면으로 돌아갑니다.</p>
+                <script>
+                    // 3000ms (3초) 뒤에 '/login' 경로로 리디렉트
+                    setTimeout(() => {
+                        window.location.href = '/login';
+                    }, 3000);
+                </script>
+          </body>
+      </html>`);
+  });
+});
+
+// 회원탈퇴 취소시
+app.post('/deleteAccount_NO', (req, res) => {
+  res.redirect('/home');
 });
 
 // 입금 기능
@@ -154,6 +294,8 @@ app.post('/output', (req, res) => {
         </html>`);
   }
 });
+
+
 
 // 서버 시작
 app.listen(3000, () => {
